0a93bca9db8aae8ba6bc6b5f7a8658a5a810c633
MinukKim <99018351+GGolDDuKi@users.noreply.github.com>
2023-07-15 11:45:41 +0900

    [Bronze I] Title: 단어 공부, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -1,25 +1,36 @@
 #include <iostream>
 using namespace std;
-int main(void) {
-    int max = 0, idx = 0, arr[26] = {0,};
-    string S;
-    const string a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
-    cin>>S;
-    for(int i = 0; i < S.length(); i++){
-        S[i] = toupper(S[i]);
-        arr[a.find(S[i])]++;
-    }
-    for(int j = 0; j < sizeof(arr)/sizeof(int); j++){
-        if(arr[j] == 0)
-            continue;
-        if(max == arr[j]){
-            idx = 26;
-            continue;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int count = 0, max = 0, max_index = 0;;
+	int c[26] = { 0 };
+	
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] < 97)
+            c[s[i] - 65]++;
+		else 
+            c[s[i] - 97]++;
+	}
+	
+	for (int i = 0; i < 26; i++)
+	{
+		if (max < c[i])
+		{
+            count = 1;
+			max = c[i];
+			max_index = i;
+		}
+        else if(max == c[i]){
+            count++;
         }
-        if(max < arr[j]){
-            max = arr[j];
-            idx = j;
-        }
-    }
-    cout<<a[idx];
+	}
+
+	if (count > 1) 
+        cout << "?";
+	else 
+        cout << (char)(max_index + 65);
 }
