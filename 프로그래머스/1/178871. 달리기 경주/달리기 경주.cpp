e3c2abd92afac308e577ffe97be48dc500021031
MinukKim <99018351+GGolDDuKi@users.noreply.github.com>
2024-03-27 20:55:13 +0900

    [level 1] Title: 달리기 경주, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+
+vector<string> solution(vector<string> players, vector<string> callings) {
+    map<string, int> playerList;
+    for(int i = 0; i < players.size(); i++){
+        playerList[players[i]] = i;
+    }
+    
+    for (int i = 0; i < callings.size(); i++)
+	{
+		string calling = callings[i];
+		int index = playerList[calling];
+
+		string player = players[index - 1];
+		players[index - 1] = players[index];
+		players[index] = player;
+
+		playerList[calling] = index - 1;
+		playerList[player] = index;
+
+	}
+    
+    return players;
+}
